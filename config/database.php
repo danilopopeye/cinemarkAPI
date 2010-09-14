ef3d17982c5546738b63a67b15d191bd9a7643bc
Danilo Sousa Gonçalves <dsgoncalves@sp.r7.com>
2010-09-14 20:48:23 -0300

    database config;

--- config/database.php
+++ config/database.php
@@ -38,9 +38,9 @@ $active_group = "default";
 $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
-$db['default']['username'] = "";
-$db['default']['password'] = "";
-$db['default']['database'] = "";
+$db['default']['username'] = "cinemark";
+$db['default']['password'] = "c1n3m4rk";
+$db['default']['database'] = "cinemark";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
 $db['default']['pconnect'] = TRUE;
