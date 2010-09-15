734ac87219c7331a223d049c506a1cfef027140e
Danilo Sousa Gonçalves <dsgoncalves@sp.r7.com>
2010-09-15 10:15:09 -0300

    removed the restserver files;

--- controllers/api.php
+++ controllers/api.php
@@ -1,13 +1,12 @@
 <?php
-require(APPPATH.'libraries/REST_Controller.php');
 
-class api extends REST_Controller {
-	function index_get(){
-		$this->response(array(
-			'params' => $this->uri->uri_to_assoc(),
-			'error' => $this->parser->getPage('http://a.localhost'),
-			'request' => $this->parser->getPage('http://is.gd')
-		), 200);
+class api extends Controller {
+	function __construct(){
+		parent::Controller();
+	}
+
+	function index(){
+		echo 'api/index';
 	}
 } 
 
