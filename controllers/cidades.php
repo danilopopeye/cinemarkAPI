d8a6b91a955631749eea58a1fd7ac3c24e4c4448
Danilo Sousa Gonçalves <dsgoncalves@sp.r7.com>
2010-09-17 16:22:48 -0300

    finished the cidades methods;

--- controllers/cidades.php
+++ controllers/cidades.php
@@ -13,6 +13,24 @@ class Cidades extends Controller {
 		));
 	}
 
+	function findById($id){
+		echo "\nargs ~ ";
+		print_r( func_get_args() );
+		$this->load->view('json', array(
+			'data' => $this->cidades->get( $id )
+		));
+	}
+
+	function find($q){
+		if( ! isset( $q['id'] ) || ! is_numeric( $q['id'] ) ){
+			return show_error('Invalid request URI', 400);
+		}
+
+		$this->load->view('json', array(
+			'data' => $this->cidades->get( $q['id'] )
+		));
+	}
+
 }
 
 /* End of file cidades.php */
--- models/mcidades.php
+++ models/mcidades.php
@@ -6,9 +6,23 @@ class MCidades extends Model {
 	}
 
 	function getAll(){
-		log_message('info', "Model: MCidades -> getAll");
+		log_message('info', "Model: MCidades -> getAll()");
 
 		return $this->db->get('cidades')->result_array();
+	}
+
+	function get($id = FALSE){
+		if( $id === FALSE ){
+			log_message('info', 'Model: MCidades -> get() ~ invalid parameter');
+
+			return array();
+		}
+
+		log_message('info', "Model: MCidades -> get(". $id .")");
+
+		return $this->db->get_where('cidades', array(
+			'id' => $id
+		) )->result_array();
 	}
 }
 
