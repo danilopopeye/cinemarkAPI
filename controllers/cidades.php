14fb844aff3cdfbe5542721252d389eec64a70fa
Danilo Sousa Gonçalves <dsgoncalves@sp.r7.com>
2010-09-27 16:42:44 -0300

    fix on the controller Cidades to work with new formats;

--- controllers/cidades.php
+++ controllers/cidades.php
@@ -1,38 +1,32 @@
 <?php
 
-class Cidades extends Controller {
+class Cidades extends REST_Controller {
 	function __construct(){
 		parent::Controller();
 
 		$this->load->model('MCidades','cidades');
 	}
 
-	function findAll($format = DEFAULT_REST_FORMAT){
-		$this->load->view($format, array(
-			'data' => $this->cidades->getAll()
-		));
+	function findAll(){
+		$this->response(
+			$this->cidades->getAll()
+		);
 	}
 
-	function findById($id,$format = DEFAULT_REST_FORMAT){
-		$this->load->view($format, array(
-			'data' => $this->cidades->get( $id )
-		));
+	function findById($id){
+		$this->response(
+			$this->cidades->get( $id )
+		);
 	}
 
 	function find($q){
 		if( ! isset( $q['id'] ) || ! is_numeric( $q['id'] ) ){
 			return show_error('Invalid request URI', 400);
 		}
-		
-		if( isset( $q['format'] ) && in_array( array('json','xml'), $q['format'] ) ){
-			$format = $q['format'];
-		} else {
-			$format = DEFAULT_REST_FORMAT;
-		}
 
-		$this->load->view($format, array(
-			'data' => $this->cidades->get( $q['id'] )
-		));
+		$this->response(
+			$this->cidades->get( $q['id'] )
+		);
 	}
 
 }
